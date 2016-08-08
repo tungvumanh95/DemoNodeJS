c8546b57be4aa57fae9f0d124692fdca88203459
Vu Manh Tung <tung.knight95@gmail.com>
2016-08-08 13:17:46 +0700

    Update HelloWorld

--- HelloWorld.js
+++ HelloWorld.js
@@ -1,1 +1,6 @@
 console.log('Hello NodeJS');
+var fs = require("fs");
+fs.readFileSync("van_ban.txt", function (err, data) {
+   console.log("Kết thúc việc đọc nội dung. Nội dung tập tin: " + data);
+});
+console.log("Đang đọc nội dung tập tin...");
